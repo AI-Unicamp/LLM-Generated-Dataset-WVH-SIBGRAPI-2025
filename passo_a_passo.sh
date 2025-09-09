004b69f0d5ea351582abd1883442ed12dda54135
ProdCor <prcpedro@hotmail.com>
2025-09-09 17:19:34 -0300

    explanation LFS files

--- passo_a_passo.sh
+++ passo_a_passo.sh
@@ -1,4 +1,8 @@
+sudo apt install git-lfs
+conda install git-lfs
+
 git clone https://github.com/AI-Unicamp/SLM-ER-Evaluation.git
+cd LLM-Generated-Dataset-WVH-SIBGRAPI-2025
 
 conda env create environment.yml
 cd scripts
