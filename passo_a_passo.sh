ba59d316b4319507e9fb75f181feee4d0949fdae
Pedro Correa <125156063+ProdCor@users.noreply.github.com>
2025-09-11 18:28:26 -0300

    Delete passo_a_passo.sh

--- passo_a_passo.sh
+++
@@ -1,9 +0,0 @@
-sudo apt install git-lfs
-conda install git-lfs
-
-git clone https://github.com/AI-Unicamp/SLM-ER-Evaluation.git
-cd LLM-Generated-Dataset-WVH-SIBGRAPI-2025
-
-conda env create environment.yml
-cd scripts
-python -m evaluation.tsne_plot.py
